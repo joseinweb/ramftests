4cb2e909742e82db39c887585a9a4c848131d9ac
joseinweb <josekutty_kuriakose@cable.comcast.com>
2025-10-24 20:40:02 +0000

    Catalog manager:Initial version

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,61 @@
 #include <iostream>
 #include "ThunderBridge.hpp"
+#include "CatalogManager.hpp"
 using namespace std;
 
 ThunderBridge thunderBridge;
+CatalogManager catalogManager;
 
-void printMainMenu() {
+void printMainMenu()
+{
     cout << "--------------------------" << endl;
     cout << "RDK App Managers Test Utility" << endl;
     cout << "Select an option:" << endl;
+
     cout << "1. Check RDK App managers status" << endl;
     cout << "2. App Manager related functions" << endl;
     cout << "3. Package Manager related functions" << endl;
+    cout << "4. Perform Catalog retrieval" << endl;
     cout << "0 . Exit" << endl;
     cout << "--------------------------" << endl;
 }
-int processUserCommands() {
 
-    while (true) {
+int processUserCommands()
+{
+
+    while (true)
+    {
         printMainMenu();
 
         int choice = retrieveInputFromUser<int>("Enter your choice: ", false, 0);
-        switch (choice) {
-            case 1:
-                cout << "Checking RDK App managers status..." << endl;
-                thunderBridge.printPluginStatus("RDK App Manager");
-                break;
-            case 2:
-                cout << "Moving to App Manager related functions..." << endl;
-                thunderBridge.showAppManagerMenu();
-                choice = -1; // Reset choice to avoid exiting
-                break;
-            case 3:
-                thunderBridge.showPackageManagerMenu();
-                choice = -1; // Reset choice to avoid exiting
-                break;
-            case 0:
-                cout << "Exiting..." << endl;
-                return 0;
-            default:
-                cout << "Invalid choice. Please try again." << endl;
+        switch (choice)
+        {
+        case 1:
+            cout << "Checking RDK App managers status..." << endl;
+            thunderBridge.printPluginStatus("RDK App Manager");
+            break;
+        case 2:
+            thunderBridge.showAppManagerMenu();
+            break;
+        case 3:
+            thunderBridge.showPackageManagerMenu();
+            break;
+        case 4:
+            catalogManager.displayMenu();
+            break;
+        case 0:
+            cout << "Exiting..." << endl;
+            return 0;
+        default:
+            cout << "Invalid choice. Please try again." << endl;
         }
     }
     return 0;
 }
-int main() {
+int main()
+{
     thunderBridge.initialize();
-    thunderBridge.connect();   
+    thunderBridge.connect();
     processUserCommands();
 
     return 0;
